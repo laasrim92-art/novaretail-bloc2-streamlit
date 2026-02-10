2abee94bc4ce806d479efa0b46c6880dd915822b
laasrim92-art <laasri.m92@gmail.com>
2026-02-10 10:53:49 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title="NovaRetail – Dashboard Marketing",
+    layout="wide"
+)
+
+st.title("📊 NovaRetail – Dashboard Marketing")
+st.markdown("""
+Analyse des performances marketing (Emailing, Google Ads, LinkedIn Ads)  
+Objectif : évaluer la génération et la qualité des leads.
+""")
+
+st.info("Dashboard en cours de construction – visualisations et KPI marketing")
